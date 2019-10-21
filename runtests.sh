cf53f18b038e42abe4525938e88b20490dc29d23
makspll <makspl17@gmail.com>
2019-10-21 12:52:38 +0100

    possible fix

--- runtests.sh
+++ runtests.sh
@@ -2,30 +2,45 @@
 
 #tests all files against pre-calculated test outputs and dumps the output to testlogs.txt
 
-. ./setup.sh
+./setup.sh
+BACKPID=$!
+wait $BACKPID
+
 "" > fails.txt
 
 echo "testing c files\n" 
 
 echo -e "1dstrfind.c\n"
-. ./test.sh -e 600 -t -f 1
+./test.sh -e 600 -t -f 1
+BACKPID=$!
+wait $BACKPID
 
 echo -e "2dstrfind.c\n"
-. ./test.sh -e 600 -t -f 2
+./test.sh -e 600 -t -f 2
+BACKPID=$!
+wait $BACKPID
 
 echo -e "wraparound.c\n" 
-. ./test.sh -e 600 -t -f 3
+./test.sh -e 600 -t -f 3
+BACKPID=$!
+wait $BACKPID
 
 echo -e "testing MIPS files\n" 
 
 echo -e "1dstrfind.s\n"
-. ./test.sh -e 600 -t -ta -f 1
+./test.sh -e 600 -t -ta -f 1
+BACKPID=$!
+wait $BACKPID
 
 echo -e "2dstrfind.s\n" 
-. ./test.sh -e 600 -t -ta -f 2
+./test.sh -e 600 -t -ta -f 2
+BACKPID=$!
+wait $BACKPID
 
 echo -e "wraparound.s\n" 
-. ./test.sh -e 600 -t -ta -f 3
+./test.sh -e 600 -t -ta -f 3
+BACKPID=$!
+wait $BACKPID
 
 echo -e "tests completed\n"
 
